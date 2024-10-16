8a2832377574e70ab4c56437959693dff910ed2b
Aigerim Sabyrkhan <aigerimsabyrkhan393@gmail.com>
2024-10-16 22:36:44 +0500

    venv recreated and add percent_change_24h, market_cap

--- frontend/src/components/CryptocurrencyCard.jsx
+++ frontend/src/components/CryptocurrencyCard.jsx
@@ -1,24 +1,46 @@
 import { Card } from 'antd';
 
+
+
+function formatNumber(num) {
+  if (num >= 1e12) {
+    return (num / 1e12).toFixed(2) + 'T'; // Триллионы
+  } else if (num >= 1e9) {
+    return (num / 1e9).toFixed(2) + 'B'; // Миллиарды
+  } else if (num >= 1e6) {
+    return (num / 1e6).toFixed(2) + 'M'; // Миллионы
+  } else {
+    return num.toFixed(2); // Если меньше миллиона
+  }
+}
+
 function CryptocurrencyCard(props) {
   const {currency} = props  
+  const  market_cap = currency.quote.USD.market_cap.toFixed(2)
+  const price = Math.round(currency.quote.USD.price)
+  const percent_change_24h = currency.quote.USD.percent_change_24h.toFixed(2)
     return (
       <div>
         <Card
+            className='border-gray-500 border-4 p-8'
             title={
                 <div className="flex items-center gap-3">
                     <img src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${currency.id}.png`}></img>
-                    <span>{currency.name}</span>
+                    <span style={{ fontSize: '24px' }}>{currency.name}</span>
                 </div>
             }
-            bordered={false}
+            
             style={{
-            width: 300,
+              width: '100%',       
+              height: 'auto',           
+              fontSize: '1.25rem',      
+              maxWidth: '600px',        
+              padding: '2rem', 
             }}
         >
-            <p>Текущая цена: {Math.round(currency.quote.USD.price)}</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <p>Текущая цена: {price}</p>
+            <p>Изменение цены за 24 часа: {percent_change_24h}%</p>
+            <p>Текущая капитализация: ${formatNumber(market_cap)}</p>
         </Card>
         
       </div>
